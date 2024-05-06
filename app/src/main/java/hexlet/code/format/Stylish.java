cdb976cd31a9ae6031ba4928cba7c2fb7ecf9794
fireyork <andrre2001@gmail.com>
2024-05-07 02:59:58 +0300

    Step fix

--- app/src/main/java/hexlet/code/format/Stylish.java
+++ app/src/main/java/hexlet/code/format/Stylish.java
@@ -7,19 +7,19 @@ public class Stylish {
     public static String render(TreeMap<String, Differences> map) {
         StringBuilder resultString = new StringBuilder("{\n");
 
-        for (var keyWithStatus : map.entrySet()) {
-            var key = keyWithStatus.getKey();
-            var data = keyWithStatus.getValue();
+        for (var keyS : map.entrySet()) {
+            var key = keyS.getKey();
+            var value = keyS.getValue();
 
-            if (data.tapDifferences().equals("unchanged")) {
-                resultString.append("    ").append(key).append(": ").append(data.getOldValue()).append("\n");
-            } else if (data.tapDifferences().equals("changed")) {
-                resultString.append(" ".repeat(2)).append("- ").append(key).append(": ").append(data.getOldValue()).append("\n");
-                resultString.append(" ".repeat(2)).append("+ ").append(key).append(": ").append(data.getNewValue()).append("\n");
-            } else if (data.tapDifferences().equals("removed")) {
-                resultString.append(" ".repeat(2)).append("- ").append(key).append(": ").append(data.getOldValue()).append("\n");
-            } else if (data.tapDifferences().equals("added")) {
-                resultString.append(" ".repeat(2)).append("+ ").append(key).append(": ").append(data.getNewValue()).append("\n");
+            if (value.tapDifferences().equals("unchanged")) {
+                resultString.append("    ").append(key).append(": ").append(value.getOldValue()).append("\n");
+            } else if (value.tapDifferences().equals("changed")) {
+                resultString.append(" ".repeat(2)).append("- ").append(key).append(": ").append(value.getOldValue()).append("\n");
+                resultString.append(" ".repeat(2)).append("+ ").append(key).append(": ").append(value.getNewValue()).append("\n");
+            } else if (value.tapDifferences().equals("removed")) {
+                resultString.append(" ".repeat(2)).append("- ").append(key).append(": ").append(value.getOldValue()).append("\n");
+            } else if (value.tapDifferences().equals("added")) {
+                resultString.append(" ".repeat(2)).append("+ ").append(key).append(": ").append(value.getNewValue()).append("\n");
             }
         }
 
