b3097af33432d132f8412e8e1a4be1a20d18d331
Andrey Piskarev <andrre2001@gmail.com>
2024-05-21 21:55:15 +0300

    Test for List map

--- app/src/main/java/hexlet/code/GetDifferences.java
+++ app/src/main/java/hexlet/code/GetDifferences.java
@@ -1,22 +1,24 @@
 package hexlet.code;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class GetDifferences {
-    public static TreeMap<String, Differences> getDifferences(
+    public static List<Map<String, Object>> getDifferences(
             Map<String, Object> data1, Map<String, Object> data2) {
         Set<String> keys = new TreeSet<>();
         keys.addAll(data1.keySet());
         keys.addAll(data2.keySet());
 
-        TreeMap<String, Differences> result = new TreeMap<>();
+        List<Map<String, Object>> resultList = new ArrayList<>();
 
         for (String key : keys) {
+            Map<String, Object> map = new HashMap<>();
             Object oldValue = data1.get(key);
             Object newValue = data2.get(key);
             String changeType = "unchanged";
@@ -29,8 +31,13 @@ public class GetDifferences {
                 changeType = "changed";
             }
 
-            result.put(key, new Differences(oldValue, newValue, changeType));
+            map.put("key", key);
+            map.put("oldValue", oldValue);
+            map.put("newValue", newValue);
+            map.put("changeType", changeType);
+
+            resultList.add(map);
         }
-        return result;
+        return resultList;
     }
 }
